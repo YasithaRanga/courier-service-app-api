822d4d1cdcecc116d7dc319040031dea5af79dbe
Yasitha Ranga <yasitharanga1@gmail.com>
2024-07-30 19:40:00 +0530

    fix: added admin account to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
@@ -6,6 +7,18 @@ async function main() {
   await prisma.role.createMany({
     data: [{ name: 'USER' }, { name: 'ADMIN' }],
     skipDuplicates: true,
+  });
+
+  const hashedPassword = await bcrypt.hash('admin', 12);
+  const userRole = await prisma.role.findUnique({ where: { name: 'ADMIN' } });
+  return await prisma.user.create({
+    data: {
+      name: 'ADMIN',
+      email: 'admin@admin.com',
+      password: hashedPassword,
+      address: '',
+      roleId: userRole?.id ?? 1,
+    },
   });
 }
 
